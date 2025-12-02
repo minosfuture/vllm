0b7d65d54b1a157cae11db1708e895a544967363
Ming Yang <minos.future@gmail.com>
2025-12-02 05:17:55 +0000

    disable chunking for flashinfer cutlass

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -742,7 +742,7 @@ class FusedMoE(CustomOp):
         return (
             self.moe_parallel_config.use_pplx_kernels
             or self.moe_parallel_config.use_deepep_ll_kernels
-            or (self.dp_size > 1 and self.use_flashinfer_cutlass_kernels)
+            #or (self.dp_size > 1 and self.use_flashinfer_cutlass_kernels)
         )
 
     @property
